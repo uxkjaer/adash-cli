2520207a27ba13dac84afdfa5de9b2ed97d2bccd
Jakob <jakob@agilux.com.au>
2021-05-26 11:33:24 +1000

    added ascii table to CLI and improved fail

--- src/helpers/fail.js
+++ src/helpers/fail.js
@@ -2,13 +2,22 @@ const chalk = require('chalk');
 const log = console.log;
 
 module.exports = (data) => {	
+	log(chalk.bold.green('SUCCESS'));
+	data.TESTS.filter((otest) => otest.STATUS !== '1-').map(test => {
+
+		
+			log('    ' + chalk.green('PASS: ') + test.TYPE + ' ' + test.NAME );
+		
+	});
 	log(chalk.bold.red('FAIL'));
-	data.TESTS.map(test => {
+	data.TESTS.filter((otest) => otest.STATUS === '1-').map(test => {
+		
 		if (test.STATUS === '1-') {
 			log('    ' + chalk.red('On: ') + test.NAME + chalk.yellow(' Test: ') + test.TEST_CLASS + '  ' + test.TEST_METHOD);
             log('    ' + chalk.red('Error: ') + chalk.white(test.FAILURE_HEADER));
             log('     ' + chalk.white(test.FAILURE_DETAILS));
             log('')
 		}
-	});		
+	})
+	
 };
--- src/helpers/pass.js
+++ src/helpers/pass.js
@@ -1,10 +1,23 @@
 let chalk = require('chalk');
+const AsciiTable = require('ascii-table');
 const log = console.log;
 
 module.exports = (data) => {
 	log(chalk.bold.green(' PASS '));
 	// @ToDo:  on verbose
+	var table = new AsciiTable('Test Summary')
+	table.setHeading('Package','Total tests','Total Success', 'Total fails','Statements Count','Statements Covered','Statements Uncovered')
+	
 	data.SUMARIES.map(test => {
-		log(chalk.green('PASS: ') + test.TYPE + ' ' + test.NAME );
+		if (test.TYPE ==='DEVC'){
+			log(chalk.green('PASS: ') + test.TYPE + ' ' + test.NAME );
+			table.addRow(test.NAME, test.TOTAL_TESTS, test.TOTAL_SUCCESS, test.TOTAL_FAILED, test.STATEMENTS_COUNT, test.STATEMENTS_COVERED, test.STATEMENTS_UNCOVERED);
+			let obj = data.SUMARIES.filter((osum) => (osum.PACKAGE_OWN = test.PACKAGE_OWN) && osum.TYPE !== 'DEVC');
+			obj.map((objSum) => {
+				table.addRow(`  - ${objSum.NAME}`, objSum.TOTAL_TESTS, objSum.TOTAL_SUCCESS, objSum.TOTAL_FAILED, objSum.STATEMENTS_COUNT, objSum.STATEMENTS_COVERED, objSum.STATEMENTS_UNCOVERED);
+			})
+		}
+		
 	});
+	log(table.toString());
 };
